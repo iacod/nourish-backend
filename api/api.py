dd4dc102213a6de4cab7c8cb96a65ceb619fd6a4
iacod <107730826+iacod@users.noreply.github.com>
2024-04-27 08:31:21 +0000

    fix serialized schema in json

--- api/api.py
+++ api/api.py
@@ -4,7 +4,6 @@ from ninja import NinjaAPI, Schema
 from django.http import HttpRequest, JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-import orjson
 
 api = NinjaAPI()
 
@@ -36,14 +35,14 @@ def register(request: HttpRequest, data: Register):
   user.set_password(data.password)
   user.save()
 
-  return JsonResponse(orjson.dumps(data), status=200)
+  return JsonResponse(data.model_dump_json(), status=200)
 
 @api.post("/login")
 def log_in(request: HttpRequest, data: Login):
   user = authenticate(request, username=data.username, password=data.password)
   if user is not None:
     login(request, user)
-    return JsonResponse(orjson.dumps(data), status=200)
+    return JsonResponse(data.model_dump_json(), status=200)
   else:
     return JsonResponse({"message": "Invalid username or password"}, status=406)
 
